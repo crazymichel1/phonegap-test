954c6a9c5f01a7d29e94e40af28b80491e8cc31b
michaelskoehler <michaelskoehler@10.10.41.8>
2013-12-06 03:02:09 +0100

    style

--- phonegap-git/public_html/js/storage/memory-store.js
+++ phonegap-git/public_html/js/storage/memory-store.js
@@ -32,21 +32,9 @@ var MemoryStore = function(successCallback, errorCallback) {
     }
 
     this.members = [
-            {"id": 1, "firstName": "Susi", "lastName": "Sorglos", "title":"Mitarbeiterin Kinderbetreuung", "managerId": 0, "city":"New York, NY", "cellPhone":"+49 157 944 999 12", "email":"susi.sorglos@arcor.net"},
-            {"id": 2, "firstName": "Michael", "lastName": "Köhler", "title":"Vorstand", "managerId": 1, "city":"Scranton, PA", "cellPhone":"570-865-2536", "officePhone":"570-123-4567", "email":"michael@dundermifflin.com"},
-            {"id": 3, "firstName": "Dwight", "lastName": "Schrute", "title":"Assistant Regional Manager", "managerId": 2, "city":"Scranton, PA", "cellPhone":"570-865-1158", "officePhone":"570-843-8963", "email":"dwight@dundermifflin.com"},
-            {"id": 4, "firstName": "Jim", "lastName": "Halpert", "title":"Assistant Regional Manager", "managerId": 2, "city":"Scranton, PA", "cellPhone":"570-865-8989", "officePhone":"570-968-5741", "email":"dwight@dundermifflin.com"},
-            {"id": 5, "firstName": "Pamela", "lastName": "Beesly", "title":"Receptionist", "managerId": 2, "city":"Scranton, PA", "cellPhone":"570-999-5555", "officePhone":"570-999-7474", "email":"pam@dundermifflin.com"},
-            {"id": 6, "firstName": "Angela", "lastName": "Martin", "title":"Senior Accountant", "managerId": 2, "city":"Scranton, PA", "cellPhone":"570-555-9696", "officePhone":"570-999-3232", "email":"angela@dundermifflin.com"},
-            {"id": 7, "firstName": "Kevin", "lastName": "Malone", "title":"Accountant", "managerId": 6, "city":"Scranton, PA", "cellPhone":"570-777-9696", "officePhone":"570-111-2525", "email":"kmalone@dundermifflin.com"},
-            {"id": 8, "firstName": "Oscar", "lastName": "Martinez", "title":"Accountant", "managerId": 6, "city":"Scranton, PA", "cellPhone":"570-321-9999", "officePhone":"570-585-3333", "email":"oscar@dundermifflin.com"},
-            {"id": 9, "firstName": "Creed", "lastName": "Bratton", "title":"Quality Assurance", "managerId": 2, "city":"Scranton, PA", "cellPhone":"570-222-6666", "officePhone":"570-333-8585", "email":"creed@dundermifflin.com"},
-            {"id": 10, "firstName": "Andy", "lastName": "Bernard", "title":"Sales Director", "managerId": 4, "city":"Scranton, PA", "cellPhone":"570-555-0000", "officePhone":"570-646-9999", "email":"andy@dundermifflin.com"},
-            {"id": 11, "firstName": "Phyllis", "lastName": "Lapin", "title":"Sales Representative", "managerId": 10, "city":"Scranton, PA", "cellPhone":"570-241-8585", "officePhone":"570-632-1919", "email":"phyllis@dundermifflin.com"},
-            {"id": 12, "firstName": "Stanley", "lastName": "Hudson", "title":"Sales Representative", "managerId": 10, "city":"Scranton, PA", "cellPhone":"570-700-6464", "officePhone":"570-787-9393", "email":"shudson@dundermifflin.com"},
-            {"id": 13, "firstName": "Meredith", "lastName": "Palmer", "title":"Supplier Relations", "managerId": 2, "city":"Scranton, PA", "cellPhone":"570-588-6567", "officePhone":"570-981-6167", "email":"meredith@dundermifflin.com"},
-            {"id": 14, "firstName": "Kelly", "lastName": "Kapoor", "title":"Customer Service Rep.", "managerId": 2, "city":"Scranton, PA", "cellPhone":"570-123-9654", "officePhone":"570-125-3666", "email":"kelly@dundermifflin.com"},
-            {"id": 15, "firstName": "Toby", "lastName": "Flenderson", "title":"Human Resources", "managerId": 1, "city":"Scranton, PA", "cellPhone":"570-485-8554", "officePhone":"570-699-5577", "email":"toby@dundermifflin.com"}
+            {"id": 1, "firstName": "Susi", "lastName": "Sorglos", "address": "Martinsburg 29 A100, 49078 Osnabrück", "phone": "+49 175 643 60 17", "landline": "", "email": "susi.sorglos@arcor.net", "dateOfBirth": "10-Feb-1992", "subgroup": "Jugend", "status": "Mitarbeiterin"},
+            {"id": 2, "firstName": "Peter", "lastName": "Lustig", "address": "Parkstr. 92, 49080 Osnabrück", "phone": "+49 158 442 312 44", "landline": "+49 541 753 132", "email": "plustig@gmx.de", "dateOfBirth": "6-Nov-1989", "subgroup": "1. Mannschaft", "status": "Mitglied"}
+     
         ];
 
     callLater(successCallback);
