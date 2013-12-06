dd6baafba759c57f2e932239d0172ce09b5ecc3f
michaelskoehler <michaelskoehler@10.10.41.8>
2013-12-06 03:39:34 +0100

    wieder websql

--- phonegap-git/public_html/js/main.js
+++ phonegap-git/public_html/js/main.js
@@ -4,7 +4,7 @@ var app = {
         var self = this;
         this.detailsURL = /^#members\/(\d{1,})/;
         this.registerEvents();
-        this.store = new MemoryStore(function() {
+        this.store = new WebSqlStore(function() {
             self.route();
         });
     },
